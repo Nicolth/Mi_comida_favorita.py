6e6440a80dc7ce32526ca5b2802347389cd45f0f
Nicolth <49917019+Nicolth@users.noreply.github.com>
2019-06-05 23:03:00 -0600

    Add files via upload

---
+++ mi_comida_favorita.py
@@ -0,0 +1,21 @@
+comidas = ["Pizza", "Hamburguesas", "Kebab",
+"Tortitas mexicanas", "Sopa de crema de tomate",
+"Tacos de lechuga", "Papas al horno",
+"Arroz con pollo frito","Estofado de carne", "Pasta",
+"Quesadila de espinacas", "Frittata de papa",
+"Chile vegetariano", "Sopa de lentejas", "Macarrones con queso",
+"Espaqueti","Vegetales asados", "Costilla Agridulce",
+"Marmaon", "Omelet", "Baleadas"]
+
+comidas_favoritas = ["Hamburguesa", "Pizza",
+"Macarrones con queso", "Costilla Agridulce", "Pasta"]
+
+def imprimir_menu():
+        print("Escoje una de las siguientes comidas: ")
+        for comida in comidas
+            print(comida)
+        print("Estas son mis cinco comidas favoritas: ")
+        for comida in comidas_favoritas:
+            print(comida)
+
+imprimir_menu()
